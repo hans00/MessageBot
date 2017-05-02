120001ab40f9010a4c201d4991be3de5e0e75290
Hans <jy29825377@gmail.com>
2017-05-02 15:38:25 +0800

    mistake

--- Features/Unlink/__init__.py
+++ Features/Unlink/__init__.py
@@ -2,5 +2,5 @@ from Group import Group
 
 def Unlink(cmds, DB):
 	Unlink.group = Group(DB)
-	cmds.setCommand('group', Link.group, """Unlink groups.""")
+	cmds.setCommand('group', Unlink.group, """Unlink groups.""")
 Unlink.group = None
